33e9368066653f7fa11dad87df12ddc45b0bb78b
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-10-07 16:16:04 +0300

    created project.js imported todo from todo.js constructor with name & description and todos
    added addTodo() & listTodo()

---
+++ project.js
@@ -0,0 +1,27 @@
+import {Todo} from './todo.js'
+
+class project {
+    constructor (name, description) {
+        this.name = String(name);
+        this.decription = String(description)
+        this.todos = []
+    }
+
+    addTodo(todo){
+        if (todo instanceof Todo){
+            this.todos.push(todo)
+            console.log(`${todo.name} added to the project`)
+        } else {
+            console.error("Iinvalid object. Only car instance can be added")
+        }
+    }
+
+    listTodos() {
+        console.log("Todos in the project")
+        this.todos.forEach(todo => {
+            console.log(`-${todo.name}`)
+        })
+    }
+
+}
+
--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-class Todo {
+ export class Todo {
     constructor(name, description, dueDate, priority, completed, notes){
 this.id = crypto.randomUUID()
 this.name = String(name);
@@ -7,5 +7,9 @@ this.dueDate = String(dueDate);
 this.priority = String(priority);
 this.completed = Boolean(completed);
 this.notes = String(notes);
+    }
+
+    toggleComplete(){
+        this.completed === true ? false : true
     }
 }
