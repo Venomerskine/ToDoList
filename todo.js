120b19855e1661aefcde0cd8178431e809252b3e
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-10-07 20:03:33 +0300

    removed data coecerion fron todo constructor included defaulting values

--- todo.js
+++ todo.js
@@ -1,12 +1,12 @@
  export class Todo {
     constructor(name, description, dueDate, priority, completed, notes){
 this.id = crypto.randomUUID()
-this.name = String(name);
-this.description = String(description);
-this.dueDate = String(dueDate);
-this.priority = String(priority);
-this.completed = Boolean(completed);
-this.notes = String(notes);
+this.name = name 
+this.description = description || "";
+this.dueDate = dueDate;
+this.priority = priority;
+this.completed = completed;
+this.notes = notes || "";
     }
 
     toggleComplete(){
