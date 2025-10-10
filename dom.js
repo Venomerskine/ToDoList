cc22dc1fd62f58a74d5ca458116040b155a10579
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-10-10 08:15:49 +0300

    On the dom.js got the app container created the renderprojects function and in it seted up the main project structure if it does not exist

--- dom.js
+++ dom.js
@@ -1,1 +1,20 @@
-// render project list to dom
+// Get app container that holds everything
+const appContainer = document.getElementById('app')
+
+// Create DOM elemnts for all projects and render them to the sidebar
+export function renderProjects(projects){
+    // Set up the main structure if it does not exist
+    if (!appContainer.querySelector('#sidebar')) {
+        appContainer.innerHTML =`
+        <header>To-Do App</header>
+        <div id="sidebar">
+            <h2>Projects</h2>
+            <ul id="project-list"><ul>
+            <button id="add-project-btn" >Add New Project</button>
+        </div>
+        <main id="todo-details">
+            <h2>Welcome! Select aproject to begin.</h2>
+        </main>
+        `
+    }
+}
